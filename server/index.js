a3c649dca794064561ec0eab6356ce4fb8de1bfb
pragyan4261 <parakhdas45@gmail.com>
2025-09-20 01:30:10 +0530

    axios added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/user.model');
